7d5b236b2f0c90cf5537d38e328d89a68ae23964
Jason Haslam <jason@scitools.com>
2019-02-25 10:43:55 -0700

    Fix failure to get more than 20 repositories from GitLab

--- src/host/GitLab.cpp
+++ src/host/GitLab.cpp
@@ -15,6 +15,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
+#include <QRegularExpression>
 #include <QUrl>
 
 namespace {
@@ -31,24 +32,52 @@ GitLab::GitLab(const QString &username)
   [this](QNetworkReply *reply) {
     reply->deleteLater();
 
-    if (reply->error() == QNetworkReply::NoError) {
-      QJsonArray array = QJsonDocument::fromJson(reply->readAll()).array();
-      for (int i = 0; i < array.size(); ++i) {
-        QJsonObject obj = array.at(i).toObject();
-        QString name = obj.value("path").toString();
-        QString fullName = obj.value("path_with_namespace").toString();
-        QString httpUrl = obj.value("http_url_to_repo").toString();
-        QString sshUrl = obj.value("ssh_url_to_repo").toString();
-
-        Repository *repo = addRepository(name, fullName);
-        repo->setUrl(Repository::Https, httpUrl);
-        repo->setUrl(Repository::Ssh, sshUrl);
-      }
-    } else {
+    if (reply->error() != QNetworkReply::NoError) {
       mError->setText(tr("Connection failed"), reply->errorString());
+      mProgress->finish();
+      return;
     }
 
-    mProgress->finish();
+    // Read repositories.
+    QJsonArray array = QJsonDocument::fromJson(reply->readAll()).array();
+    for (int i = 0; i < array.size(); ++i) {
+      QJsonObject obj = array.at(i).toObject();
+      QString name = obj.value("path").toString();
+      QString fullName = obj.value("path_with_namespace").toString();
+      QString httpUrl = obj.value("http_url_to_repo").toString();
+      QString sshUrl = obj.value("ssh_url_to_repo").toString();
+
+      Repository *repo = addRepository(name, fullName);
+      repo->setUrl(Repository::Https, httpUrl);
+      repo->setUrl(Repository::Ssh, sshUrl);
+    }
+
+    // Check for additional pages.
+    QString link = reply->rawHeader("Link");
+    if (link.isEmpty()) {
+      mProgress->finish();
+      return;
+    }
+
+    QMap<QString,QString> map;
+    QRegularExpression re("<(.*)>; rel=\"(\\w+)\"");
+    foreach (const QString &record, link.split(", ")) {
+      QRegularExpressionMatch match = re.match(record);
+      if (match.isValid() && match.hasMatch())
+        map.insert(match.captured(2), match.captured(1));
+    }
+
+    QString next = map.value("next");
+    if (next.isEmpty()) {
+      mProgress->finish();
+      return;
+    }
+
+    // Request next page.
+    QNetworkRequest request(next);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, kContentType);
+    mMgr.get(request);
+    startProgress();
   });
 }
 
