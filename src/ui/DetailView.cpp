c33366c2859a268a483ad04bbc44a84614d60601
Jeremy Whiting <jpwhiting@kde.org>
2019-07-31 20:34:44 -0600

    If no detailSplitter sizes were saved set a sane default.
    I didn't measure exactly, but these sizes seem to match the existing
    sizes of the top and bottom of the DetailView. If needed these
    defaults can be adjusted.

--- src/ui/DetailView.cpp
+++ src/ui/DetailView.cpp
@@ -52,6 +52,8 @@ namespace {
 
 const int kSize = 64;
 const QString kSplitterKey = "detailSplitter";
+const int kSplitterTop = 120;
+const int kSplitterBottom = 1000;
 const char *kCacheKey = "cache_key";
 const QString kRangeFmt = "%1..%2";
 const QString kDateRangeFmt = "%1-%2";
@@ -1170,8 +1172,14 @@ DetailView::DetailView(const git::Repository &repo, QWidget *parent)
   mContent->addWidget(new TreeWidget(repo, this));
 
   // Restore splitter state.
-  splitter->restoreState(QSettings().value(kSplitterKey).toByteArray());
-
+  if (QSettings().contains(kSplitterKey)) {
+    splitter->restoreState(QSettings().value(kSplitterKey).toByteArray());
+  } else {
+    // set a default size
+    QList<int> sizes;
+    sizes << kSplitterTop << kSplitterBottom;
+    splitter->setSizes(sizes);
+  }
 }
 
 DetailView::~DetailView() {}
