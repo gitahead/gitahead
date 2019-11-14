ce17eaa32218876aa2018cfb2beafafa40bf632c
kas <kas-luthor@users.noreply.github.com>
2019-11-14 18:46:08 +0100

    Adds support for customizable hotkeys

---
+++ src/ui/HotkeyManager.cpp
@@ -0,0 +1,179 @@
+//
+//          Copyright (c) 2016, Scientific Toolworks, Inc.
+//
+// This software is licensed under the MIT License. The LICENSE.md file
+// describes the conditions under which this software may be distributed.
+//
+// Author: Kas
+//
+
+#include "HotkeyManager.h"
+
+struct HotkeyManagerHandle
+{
+  const char *defaultKeys;
+  QKeySequence::StandardKey standardKey;
+  const char *configPath;
+  const char *label;
+  int index;
+  HotkeyManagerHandle *next;
+};
+
+static HotkeyManagerHandle *hotkeyRegistry;
+
+HotkeyHandle::HotkeyHandle(QMetaObject::Connection &&changeSignalConnection): changeSignalConnection(std::move(changeSignalConnection))
+{
+}
+
+HotkeyHandle::~HotkeyHandle()
+{
+  disconnect(changeSignalConnection);
+}
+
+HotkeyHandle *Hotkey::use(std::function<void(const QKeySequence&)> changeHandler, HotkeyManager *manager) const
+{
+  if (!manager)
+    manager = HotkeyManager::instance();
+
+  QMetaObject::Connection connection = QObject::connect(manager, &HotkeyManager::changed, [this, changeHandler, manager](HotkeyManagerHandle *handle) {
+    if (handle == mHandle)
+      changeHandler(manager->keys(mHandle));
+  });
+
+  changeHandler(manager->keys(mHandle));
+
+  return new HotkeyHandle(std::move(connection));
+}
+
+HotkeyHandle *Hotkey::use(QAction *action, HotkeyManager *manager) const
+{
+  HotkeyHandle *res = use(
+    [action](const QKeySequence &keys) {
+      action->setShortcut(keys);
+    },
+    manager
+  );
+
+  res->setParent(action);
+
+  return res;
+}
+
+QString Hotkey::label() const
+{
+  return QObject::tr(mHandle->label);
+}
+
+QKeySequence Hotkey::currentKeys(const HotkeyManager *manager) const
+{
+  if (!manager)
+    manager = HotkeyManager::instance();
+
+  return manager->keys(mHandle);
+}
+
+QString Hotkey::defaultKeys() const
+{
+  return QObject::tr(mHandle->defaultKeys);
+}
+
+void Hotkey::setKeys(const QKeySequence &keys, HotkeyManager *manager)
+{
+  if (!manager)
+    manager = HotkeyManager::instance();
+
+  manager->setKeys(mHandle, keys);
+}
+
+Hotkey::Hotkey(const HotkeyManagerHandle *handle): mHandle(handle)
+{
+}
+
+HotkeyManager *HotkeyManager::instance()
+{
+  static HotkeyManager *instance = nullptr;
+  if (!instance)
+    instance = new HotkeyManager();
+
+  return instance;
+}
+
+Hotkey HotkeyManager::registerHotkey(const char *defaultKeys, QKeySequence::StandardKey standardKey, const char *configPath, const char *label)
+{
+  HotkeyManagerHandle *info = new HotkeyManagerHandle();
+  info->defaultKeys = defaultKeys;
+  info->standardKey = standardKey;
+  info->configPath = configPath;
+  info->label = label;
+  info->next = hotkeyRegistry;
+
+  if (hotkeyRegistry)
+    info->index = hotkeyRegistry->index + 1;
+  else
+    info->index = 0;
+
+  hotkeyRegistry = info;
+
+  return Hotkey(info);
+}
+
+Hotkey HotkeyManager::registerHotkey(const char *defaultKeys, const char *configPath, const char *label)
+{
+  if (!defaultKeys)
+    defaultKeys = "";
+
+  return registerHotkey(defaultKeys, QKeySequence::StandardKey::UnknownKey, configPath, label);
+}
+
+Hotkey HotkeyManager::registerHotkey(QKeySequence::StandardKey standardKey, const char *configPath, const char *label)
+{
+  return registerHotkey(nullptr, standardKey, configPath, label);
+}
+
+static QKeySequence getKeys(const QString &setting, HotkeyManagerHandle *handle)
+{
+  if (!setting.isEmpty())
+    return QKeySequence(setting, QKeySequence::SequenceFormat::PortableText);
+  else if (!handle->defaultKeys)
+    return QKeySequence(handle->standardKey);
+  else
+    return QKeySequence(handle->defaultKeys);
+}
+
+HotkeyManager::HotkeyManager(Settings *settings):
+  mSettings(settings),
+  mHandles(hotkeyRegistry->index + 1),
+  mKeys(hotkeyRegistry->index + 1)
+{
+  if (!mSettings)
+    mSettings = Settings::instance();
+
+  mSettings->beginGroup("hotkeys");
+
+  for (HotkeyManagerHandle *i = hotkeyRegistry; i; i = i->next) {
+    mHandles[i->index] = i;
+    mKeys[i->index] = getKeys(mSettings->value(i->configPath, "").toString(), i);
+  }
+
+  mSettings->endGroup();
+}
+
+QVector<Hotkey> HotkeyManager::knownHotkeys() const
+{
+  QVector<Hotkey> res(mHandles.size());
+
+  for (HotkeyManagerHandle *i: mHandles)
+    res[i->index] = Hotkey(i);
+
+  return res;
+}
+
+QKeySequence HotkeyManager::keys(const HotkeyManagerHandle *handle) const
+{
+  return mKeys[handle->index];
+}
+
+void HotkeyManager::setKeys(const HotkeyManagerHandle *handle, const QKeySequence &keys)
+{
+  mKeys[handle->index] = keys;
+}
---
+++ src/ui/HotkeyManager.h
@@ -0,0 +1,84 @@
+//
+//          Copyright (c) 2016, Scientific Toolworks, Inc.
+//
+// This software is licensed under the MIT License. The LICENSE.md file
+// describes the conditions under which this software may be distributed.
+//
+// Author: Kas
+//
+
+#ifndef HOTKEYMANAGER_H
+#define HOTKEYMANAGER_H
+
+#include "conf/Settings.h"
+#include <functional>
+#include <QAction>
+#include <QObject>
+#include <QKeySequence>
+#include <QString>
+#include <QVector>
+
+class HotkeyManager;
+class HotkeyManagerHandle;
+
+class HotkeyHandle : public QObject
+{
+public:
+  HotkeyHandle(QMetaObject::Connection &&changeSignalConnection);
+  ~HotkeyHandle();
+
+private:
+  QMetaObject::Connection changeSignalConnection;
+};
+
+class Hotkey
+{
+  friend class HotkeyManager;
+public:
+  inline Hotkey(): mHandle(nullptr)
+  {
+  }
+
+  HotkeyHandle *use(std::function<void(const QKeySequence&)> changeHandler, HotkeyManager *manager = nullptr) const;
+  HotkeyHandle *use(QAction *action, HotkeyManager *manager = nullptr) const;
+
+  QString label() const;
+  QKeySequence currentKeys(const HotkeyManager *manager = nullptr) const;
+  QString defaultKeys() const;
+  void setKeys(const QKeySequence &keys, HotkeyManager *manager = nullptr);
+
+private:
+  const HotkeyManagerHandle *mHandle;
+
+  Hotkey(const HotkeyManagerHandle *handle);
+};
+
+class HotkeyManager : public QObject
+{
+  friend class Hotkey;
+
+  Q_OBJECT
+
+public:
+  static Hotkey registerHotkey(const char *defaultKeys, const char *configPath, const char *label);
+  static Hotkey registerHotkey(QKeySequence::StandardKey defaultKeys, const char *configPath, const char *label);
+  static HotkeyManager *instance();
+
+  HotkeyManager(Settings *settings = nullptr);
+  QVector<Hotkey> knownHotkeys() const;
+
+signals:
+  void changed(HotkeyManagerHandle *handle);
+
+private:
+  static Hotkey registerHotkey(const char *defaultKeys, QKeySequence::StandardKey standardKey, const char *configPath, const char *label);
+
+  Settings *mSettings;
+  QVector<HotkeyManagerHandle*> mHandles;
+  QVector<QKeySequence> mKeys;
+
+  QKeySequence keys(const HotkeyManagerHandle *handle) const;
+  void setKeys(const HotkeyManagerHandle *handle, const QKeySequence &keys);
+};
+
+#endif
