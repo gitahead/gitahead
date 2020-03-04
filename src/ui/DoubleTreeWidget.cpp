338de1670f124bc374af6865e4f11e703b3534d1
Martin Marmsoler <martin.marmsoler@gmail.com>
2020-03-04 19:12:56 +0100

    add cleaner layout.
    add buttons to stage/unstage everything

--- src/ui/DoubleTreeWidget.cpp
+++ src/ui/DoubleTreeWidget.cpp
@@ -12,16 +12,41 @@
 
 #include <QTreeView>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 
 DoubleTreeWidget::DoubleTreeWidget(const git::Repository &repo, QWidget *parent)
   : ContentWidget(parent)
 {
+	QVBoxLayout* vBoxLayout = new QVBoxLayout();
+	QHBoxLayout* hBoxLayout = new QHBoxLayout();
+	QLabel* label = new QLabel(tr("Staged Files"));
+	QPushButton* button = new QPushButton(tr("Stage all changes"));
+	hBoxLayout->addWidget(label);
+	hBoxLayout->addWidget(button);
 	stagedFiles = new QTreeView(this);
+	vBoxLayout->addLayout(hBoxLayout);
+	vBoxLayout->addWidget(stagedFiles);
+	QWidget* stagedWidget = new QWidget();
+	stagedWidget->setLayout(vBoxLayout);
+
+	vBoxLayout = new QVBoxLayout();
+	hBoxLayout = new QHBoxLayout();
+	label = new QLabel(tr("Unstaged Files"));
+	button = new QPushButton(tr("Unstage all changes"));
+	hBoxLayout->addWidget(label);
+	hBoxLayout->addWidget(button);
 	unstagedFiles = new QTreeView(this);
+	vBoxLayout->addLayout(hBoxLayout);
+	vBoxLayout->addWidget(unstagedFiles);
+	QWidget* unstagedWidget = new QWidget();
+	unstagedWidget->setLayout(vBoxLayout);
+
 	QSplitter *treeViewSplitter = new QSplitter(Qt::Vertical, this);
 	treeViewSplitter->setHandleWidth(10);
-	treeViewSplitter->addWidget(unstagedFiles);
-	treeViewSplitter->addWidget(stagedFiles);
+	treeViewSplitter->addWidget(unstagedWidget);
+	treeViewSplitter->addWidget(stagedWidget);
 	treeViewSplitter->setStretchFactor(1, 1);
 
 	mEditor = new BlameEditor(repo, this);
@@ -29,13 +54,15 @@ DoubleTreeWidget::DoubleTreeWidget(const git::Repository &repo, QWidget *parent)
 
 	QSplitter* splitter = new QSplitter(Qt::Horizontal, this);
 	splitter->setHandleWidth(0);
-	splitter->addWidget(treeViewSplitter);
 	splitter->addWidget(mEditor);
+	splitter->addWidget(treeViewSplitter);
 	splitter->setStretchFactor(1, 1);
 
 	QVBoxLayout* layout = new QVBoxLayout(this);
 	layout->setContentsMargins(0,0,0,0);
 	layout->addWidget(splitter);
+
+	setLayout(layout);
 }
 
 QString DoubleTreeWidget::selectedFile() const {
