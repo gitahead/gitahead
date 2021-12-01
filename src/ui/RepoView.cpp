f9f4d9b5649f7af9af2c4b024953b8fbf9bd7193
Martin Marmsoler <martin.marmsoler@brainlab.com>
2021-12-01 11:09:21 +0100

    fix Indexer installation path for the flatpak package

--- src/ui/RepoView.cpp
+++ src/ui/RepoView.cpp
@@ -858,7 +858,12 @@ void RepoView::startIndexing()
     args.prepend("--log");
 
   QDir dir(QCoreApplication::applicationDirPath());
-  mIndexer.start(dir.filePath("indexer"), args);
+  auto indexer_cmd = dir.filePath("indexer");
+  QFileInfo check_file(indexer_cmd);
+  if (!check_file.isFile()) {
+	  qDebug() << "No indexer found: " << indexer_cmd;
+  }
+  mIndexer.start(indexer_cmd, args);
 }
 
 void RepoView::cancelIndexing()
