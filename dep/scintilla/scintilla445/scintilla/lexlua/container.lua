7db6af4269261d50d035b3c9d3c46c34686ca3ad
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-01-27 17:37:09 +0100

    - update scintilla version which fixes building with Xcode 12 - use submodule from official repository which exists now

--- dep/scintilla/scintilla445/scintilla/lexlua/container.lua
+++
@@ -1,5 +0,0 @@
--- Copyright 2006-2020 Mitchell mitchell.att.foicica.com. See License.txt.
--- Container LPeg lexer.
--- This is SciTE's plain text lexer.
-
-return require('lexer').new('container')
--- dep/scintilla/scintilla445/scintilla/scripts/archive.sh
+++
@@ -1,5 +0,0 @@
-# Up to parent directory of scintilla
-cd ../..
-
-# Archive Scintilla to scintilla.tgz
-hg archive --repository scintilla scintilla.tgz
