dfa619164b5dcbcd42fe921f4ad4600bb4fcf7d4
Murmele <Murmele@users.noreply.github.com>
2022-02-04 20:21:23 +0100

    Update src/editor/TextEditor.cpp
    Co-authored-by: stefanknotzer <67198194+stefanknotzer@users.noreply.github.com>

--- src/editor/TextEditor.cpp
+++ src/editor/TextEditor.cpp
@@ -46,7 +46,7 @@ QPixmap stagedUnstagedIcon(
   checkBox.setStyle(QStyleFactory::create("windows"));
 #endif
   checkBox.setStyleSheet(checkBoxStyle.arg(background.name()).arg(fontHeight - 2));
-  return checkBox.grab(QRect(QPoint(0,0), QSize(fontHeight, fontHeight)));
+  return checkBox.grab(QRect(QPoint(0,0), QSize(fontHeight - 2, fontHeight - 2)));
 }
 
 } // anon. namespace
