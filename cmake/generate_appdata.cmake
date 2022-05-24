be1ecb6a57ae04c0891c7eb733ac73f4e3fa7ad3
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-05-24 13:55:04 +0200

    add description child

--- cmake/generate_appdata.cmake
+++ cmake/generate_appdata.cmake
@@ -1,6 +1,6 @@
 # add release notes to the appdata file
 file(READ "${CHANGELOG_HTML}" HTML_CHANGELOGS)
 string(REPLACE "\n" "\n\t" RELEASES ${HTML_CHANGELOGS}) # add tabulator
-string(REGEX REPLACE "<h3>(v[1-9]\\.[0-9]\\.[0-9]) - ([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])<\\/h3>" "<release version='\\1' date='\\2'>" RELEASES ${RELEASES})
-string(REGEX REPLACE "<hr \\/>" "</release>" RELEASES ${RELEASES})
+string(REGEX REPLACE "<h3>(v[1-9]\\.[0-9]\\.[0-9]) - ([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])<\\/h3>" "<release version='\\1' date='\\2'>\n\t<description>" RELEASES ${RELEASES})
+string(REGEX REPLACE "<hr \\/>" "</description>\n\t</release>" RELEASES ${RELEASES})
 configure_file(${APPDATA_CONF} ${APPDATA})
