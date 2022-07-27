6796c42408dc8212b3895b7999c756b8b03a734c
kas <exactly-one-kas@users.noreply.github.com>
2022-07-27 18:11:16 +0200

    Fix crash when global GIT config is invalid

--- src/app/Gittyup.cpp
+++ src/app/Gittyup.cpp
@@ -8,7 +8,9 @@
 //
 
 #include "Application.h"
+#include "git/Config.h"
 #include "ui/MainWindow.h"
+#include <QMessageBox>
 
 int main(int argc, char *argv[]) {
   Application::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -18,6 +20,13 @@ int main(int argc, char *argv[]) {
   // Check if only one running instance is allowed and already running
   if (app.runSingleInstance())
     return 0;
+
+  if (!git::Config::global().isValid()) {
+    QMessageBox::warning(
+        nullptr, GITTYUP_NAME,
+        QObject::tr("Your global GIT configuration is invalid, Gittyup won't "
+                    "run properly until this is fixed"));
+  }
 
   // Restore windows before checking for updates so that
   // the update dialog pops up on top of the other windows.
