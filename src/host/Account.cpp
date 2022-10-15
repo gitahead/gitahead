8fd8644bd3106f0ba8f3addb362ac739d597c103
Odin Vex <44311901+OdinVex@users.noreply.github.com>
2022-10-15 19:31:57 -0400

    Add Gitea Support
    This is a rough, initial attempt at adding Gitea support. Some features have been tested similarly to GitHub.
    
    Signed-off-by: Odin Vex <44311901+OdinVex@users.noreply.github.com>

--- src/host/Account.cpp
+++ src/host/Account.cpp
@@ -11,6 +11,7 @@
 #include "Beanstalk.h"
 #include "Bitbucket.h"
 #include "GitHub.h"
+#include "Gitea.h"
 #include "GitLab.h"
 #include "cred/CredentialHelper.h"
 #include <QFileInfo>
@@ -136,6 +137,9 @@ QIcon Account::icon(Kind kind) {
     case Account::GitHub:
       name = "github";
       break;
+    case Account::Gitea:
+      name = "gitea";
+      break;
     case Account::Bitbucket:
       name = "bitbucket";
       break;
@@ -184,6 +188,10 @@ QString Account::helpText(Kind kind) {
           "command-line/'>personal access token</a> in the password field "
           "instead.");
 
+    case Gitea:
+      return tr(
+          "<b>Note:</b> Only Basic authentication is currently supported ");
+
     case GitLab:
       return tr("<b>Note:</b> Basic authentication is not supported. Use a "
                 "<a href='https://docs.gitlab.com/ee/user/profile/personal_"
@@ -200,6 +208,8 @@ QString Account::defaultUrl(Kind kind) {
   switch (kind) {
     case GitHub:
       return GitHub::defaultUrl();
+    case Gitea:
+      return Gitea::defaultUrl();
     case Bitbucket:
       return Bitbucket::defaultUrl();
     case Beanstalk:
