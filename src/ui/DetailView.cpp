70478d86ab1e6c048e712af9aac60b4d41d9fd5c
jens <98707820+codeschrat@users.noreply.github.com>
2022-11-12 14:26:07 +0100

    Show avatars only if respective setting is true

--- src/ui/DetailView.cpp
+++ src/ui/DetailView.cpp
@@ -414,23 +414,26 @@ public:
     QString msg = commit.message(git::Commit::SubstituteEmoji).trimmed();
     mMessage->setPlainText(msg);
 
-    auto w = window();
-    auto w_handler = w->windowHandle();
+    const bool showAvatars = Settings::instance()->value("window/view/avatarsVisible").toBool();
+    if (showAvatars) {
+      auto w = window();
+      auto w_handler = w->windowHandle();
 
-    int size = kSize * w_handler->devicePixelRatio();
-    QByteArray email = commit.author().email().trimmed().toLower().toUtf8();
-    QByteArray hash = QCryptographicHash::hash(email, QCryptographicHash::Md5);
+      int size = kSize * w_handler->devicePixelRatio();
+      QByteArray email = commit.author().email().trimmed().toLower().toUtf8();
+      QByteArray hash = QCryptographicHash::hash(email, QCryptographicHash::Md5);
 
-    // Check the cache first.
-    QByteArray key = hash.toHex() + '@' + QByteArray::number(size);
-    mPicture->setPixmap(mCache.value(key));
+      // Check the cache first.
+      QByteArray key = hash.toHex() + '@' + QByteArray::number(size);
+      mPicture->setPixmap(mCache.value(key));
 
-    // Request the image from gravatar.
-    if (!mCache.contains(key)) {
-      QUrl url(
-          kUrl.arg(QString::fromUtf8(hash.toHex()), QString::number(size)));
-      QNetworkReply *reply = mMgr.get(QNetworkRequest(url));
-      reply->setProperty(kCacheKey, key);
+      // Request the image from gravatar.
+      if (!mCache.contains(key)) {
+        QUrl url(
+            kUrl.arg(QString::fromUtf8(hash.toHex()), QString::number(size)));
+        QNetworkReply *reply = mMgr.get(QNetworkRequest(url));
+        reply->setProperty(kCacheKey, key);
+      }
     }
 
     // Remember the id.
